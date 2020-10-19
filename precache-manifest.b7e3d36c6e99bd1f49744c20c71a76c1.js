fd551935b48528556aeadc879e40015e2d276054
hellpnick <48753830+hellhellpnick@users.noreply.github.com>
2020-10-19 17:57:51 +0300

    Updates

---
+++ precache-manifest.b7e3d36c6e99bd1f49744c20c71a76c1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92295f5e584f06341f650478d9c257e1",
+    "url": "/react-form/index.html"
+  },
+  {
+    "revision": "ea4ed7a657c437bab52c",
+    "url": "/react-form/static/css/main.e1223660.chunk.css"
+  },
+  {
+    "revision": "4fb70f04a75fcdc07cd6",
+    "url": "/react-form/static/js/2.af8bfa1d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-form/static/js/2.af8bfa1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4ed7a657c437bab52c",
+    "url": "/react-form/static/js/main.acf2327d.chunk.js"
+  },
+  {
+    "revision": "214ddc92707c3bb084c6",
+    "url": "/react-form/static/js/runtime-main.5d4a941f.js"
+  },
+  {
+    "revision": "92134c00d1c4818c60fb3827a388fd53",
+    "url": "/react-form/static/media/background.92134c00.png"
+  },
+  {
+    "revision": "d6f9dd03b677c134d64bdb68fb3a1736",
+    "url": "/react-form/static/media/laptop.d6f9dd03.svg"
+  },
+  {
+    "revision": "2d2b4b177c6945ddb1bcedcc3ec09aa2",
+    "url": "/react-form/static/media/mess.2d2b4b17.svg"
+  }
+]);
